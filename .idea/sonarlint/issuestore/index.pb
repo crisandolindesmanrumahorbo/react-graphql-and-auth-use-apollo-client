aa1c214110e1bc93b2b05c3b9b0a634189b25444
Crisandolin Rumahorbo <crisandolin.rumahorbo@btpn.com>
2020-09-01 15:30:08 +0700

    first commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,14 @@
+
+9
+	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+<
+src/index.js,f/9/f900edc44cb929c2267a16100a32851f6d5b477d
+:
+
+src/App.js,1/e/1e43fa63aecd2127be034b3fb9d4caa172275714
+<
+package.json,7/0/7030d0b2f71b999ff89a343de08c414af32fc93a
+A
+package-lock.json,f/a/fa288d1472d29beccb489a676f68739ad365fc47
+D
+src/serviceWorker.js,9/2/920ab6ab4a57ee20a36df4e3e2e73b70f9969d14
--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { ApolloProvider } from '@apollo/client';
+import { gql } from '@apollo/client';
+import {setContext} from "@apollo/client/link/context";
+
+const httpLink = createHttpLink({
+    uri: 'http://localhost:8081/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+    // get the authentication token from local storage if it exists
+    const token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhcmllZkBkb3dpdGguY29tIiwidXNlcklkIjoxLCJpYXQiOjE1OTczOTY4MTB9.RlhOP7M6PECtqLm683QPIoinv-zMV4GPO8p54D0SPrp6AVSngWHc6QmTu5pHASSoQm_zfD2WfUFZo95jFo6p8A"
+    // return the headers to the context so httpLink can read them
+    return {
+        headers: {
+            ...headers,
+            authorization: token ? `Bearer ${token}` : "",
+        }
+    }
+});
+
+const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache()
+});
+
+const testQuery = gql`
+  query getSpesificVehicle($id: Int!){
+    vehicle(id: $id){
+      type
+      modelCode
+    }
+  }
+`;
+
+client
+    .query({
+      query: testQuery,
+      variables: {
+          id: 4
+      }
+    })
+    .then(result => console.log(result));
+
+const testMutation = gql`
+  mutation createVehicle($vehicle: VehicleWrapper){
+    createVehicle(vehicle: $vehicle){
+      id
+      type
+      modelCode
+      brandName
+    }
+  }
+    `;
+
+client.mutate({
+  mutation: testMutation,
+  variables: {
+    vehicle:{
+      type: "car",
+      modelCode: "carr",
+      brandName: "String"
+    }
+  }
+}).then(result => console.log(result));
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <ApolloProvider client={client}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+      </ApolloProvider>
   );
 }
 
